ea12900f8d5a20b8f9cf94f89e3d79feb8ba8e46
Chuks Emenike <chukschuksemenike@gmail.com>
2023-07-08 19:59:15 +0200

    created acm file

--- variables.tf
+++ variables.tf
@@ -76,3 +76,14 @@ variable "ssh_location" {
   #description = "create a standby db instance"
   #type        = bool
 #}
+
+# acm variables
+variable "domain_name" {
+  description = "domain name"
+  type        = string
+}
+
+variable "alternative_names" {
+  description = "sub domain name"
+  type        = string
+}
